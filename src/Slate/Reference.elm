68f8bc870f09db18a87d9c4e2f5880a773a709d9
Charles Scalfani <charles@panosoft.com>
2017-01-17 10:26:54 -0800

    Use ?=

--- src/Slate/Reference.elm
+++ src/Slate/Reference.elm
@@ -4,15 +4,11 @@ import Dict exposing (Dict)
 import Json.Encode as JE exposing (..)
 import Json.Decode as JD exposing (..)
 import Slate.Event exposing (Event)
+import Utils.Ops exposing ((?=))
 
 
 type alias EntityReference =
     String
-
-
-(//) : Maybe a -> a -> a
-(//) =
-    flip Maybe.withDefault
 
 
 lookupEntity : Dict String entity -> Event -> entity -> entity
@@ -22,7 +18,7 @@ lookupEntity entities event default =
 
 dereferenceEntity : Dict String entity -> Maybe EntityReference -> entity -> entity
 dereferenceEntity entities ref default =
-    Dict.get (ref // "") entities // default
+    Dict.get (ref ?= "") entities ?= default
 
 
 entityReferenceEncode : EntityReference -> JE.Value
